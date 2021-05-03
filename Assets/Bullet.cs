e509750472228819b90f407c271304b4d1adffd3
Laith Nurie <laithnurie@gmail.com>
2021-05-03 13:22:57 +0100

    laser shooter initial, ship shooting done

---
+++ Assets/Bullet.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    private float bulletHeight;
+    private float yMax;
+
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        bulletHeight = spriteRenderer.bounds.size.y;
+
+        Camera gameCamera = Camera.main;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y + bulletHeight;
+    }
+
+    void Update()
+    {
+        Travel();
+    }
+
+    private void Travel()
+    {
+        float deltaY = Time.deltaTime * moveSpeed;
+        float newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(transform.position.x, newYPos);
+
+        if (newYPos > yMax)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] GameObject bullet;
+
+    float xMin;
+    float xMax;
+
+    float yMin;
+    float yMax;
+
+    float playerWidth;
+    float playerHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        playerWidth = spriteRenderer.bounds.size.x;
+        playerHeight = spriteRenderer.bounds.size.y;
+        SetupMoveBoundries();
+    }
+
+    private void SetupMoveBoundries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + (playerWidth / 2);
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - (playerWidth / 2);
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + (playerHeight / 2);
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - (playerHeight / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        PrepareWeapons();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void PrepareWeapons()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+}
